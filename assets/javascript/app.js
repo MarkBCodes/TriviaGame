13a9a83cb04c9afcb3ac068e2bb27dcb14cfac83
Mark B <mark.bullard42@gmail.com>
2019-06-12 22:24:02 -0400

    Added event listeners and trivia properties.

---
+++ assets/javascript/app.js
@@ -0,0 +1,17 @@
+$(document).ready(function() {
+  // event listeners
+  $("#remaining-time").hide();
+  $("#start").on("click", trivia.startGame);
+  $(document).on("click", ".option", trivia.guessChecker);
+});
+
+var trivia = {
+  // trivia properties
+  correct: 0,
+  incorrect: 0,
+  unanswered: 0,
+  currentSet: 0,
+  timer: 20,
+  timerOn: false,
+  timerId: ""
+};
